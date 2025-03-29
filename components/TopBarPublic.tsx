35afc40d9e65b9c6d522c171080fdea874b5353a
sebsoler212 <sebsoler212@gmail.com>
2025-03-29 11:48:20 -0400

    small edits

--- components/TopBarPublic.tsx
+++ components/TopBarPublic.tsx
@@ -1,11 +1,17 @@
 'use client';
 import Image from 'next/image'
+import { useRouter } from "next/navigation"
 
 const TopBarPublic = () => {
 
+  const router = useRouter()
+  const goHome = () => {
+    router.push('/')
+  }
+
   return (
     <header className="fixed top-0 w-full bg-gray-800 text-white py-6 z-30">
-        <div className="fixed top-2 left-4 cursor-pointer z-40 flex">
+        <div className="fixed top-2 left-4 cursor-pointer z-40 flex" onClick={goHome}>
           <Image 
           src="/glogo.png"
           alt="Group Swap"
@@ -13,7 +19,7 @@ const TopBarPublic = () => {
           width={40}
           height={30}
           />
-          <span className="mt-1 ml-1">Group Swap</span>
+          <span className="mt-1 ml-1 font-bold">Group Swap</span>
         </div>
         <div className="fixed top-[18px] md:top-[14px] right-4 text-slate-600 text-xs md:text-sm text-white cursor-pointer z-40">
           <a href="/#packs" className="px-2">Photo Packs</a>
